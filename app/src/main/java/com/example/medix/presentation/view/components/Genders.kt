2b8fdd22fc3a82eefb82764699c61b25ee42caae
Tarif <abdo.tareef10@gmail.com>
2024-05-04 00:03:55 +0300

    in progress

---
+++ app/src/main/java/com/example/medix/presentation/view/components/Genders.kt
@@ -0,0 +1,91 @@
+package com.example.medix.presentation.view.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.RadioButtonColors
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.medix.ui.theme.blackText
+import com.example.medix.ui.theme.lightBackground
+import com.example.medix.ui.theme.orange
+
+enum class Gender { MALE, FEMALE}
+
+@Composable
+fun GenderSelection(onGenderSelected: (Gender) -> Unit) {
+    var selectedGender by remember { mutableStateOf<Gender?>(null) }
+
+    Row(
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        RadioGroup(selectedGender) { gender ->
+            selectedGender = gender
+            onGenderSelected(gender)
+        }
+    }
+}
+
+@Composable
+fun RadioGroup(selectedGender: Gender?, onGenderSelected: (Gender) -> Unit) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        RadioButton(
+            selected = selectedGender == Gender.MALE,
+            onClick = { onGenderSelected(Gender.MALE) },
+            colors = RadioButtonColors(
+                unselectedColor = lightBackground,
+                selectedColor = orange,
+                disabledSelectedColor = orange,
+                disabledUnselectedColor = lightBackground
+            )
+        )
+
+        Text(
+            text = "Male",
+            fontWeight = FontWeight.SemiBold,
+            fontSize = 16.sp,
+            color = blackText
+        )
+
+        Spacer(modifier = Modifier.width(30.dp))
+
+        RadioButton(
+            selected = selectedGender == Gender.FEMALE,
+            onClick = { onGenderSelected(Gender.FEMALE) },
+            colors = RadioButtonColors(
+                unselectedColor = lightBackground,
+                selectedColor = orange,
+                disabledSelectedColor = orange,
+                disabledUnselectedColor = lightBackground
+            )
+        )
+
+        Text(
+            text = "Female",
+            fontWeight = FontWeight.SemiBold,
+            fontSize = 16.sp,
+            color = blackText
+        )
+    }
+}
